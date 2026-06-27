c7754e3c42c22f8d8fbbd65feaebfd761d6755f7
ashrafulasif123 <cseasifiiuc9@gmail.com>
2026-06-27 17:38:51 +0600

    mongodb connect

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { ServerApiVersion, MongoClient } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+/** client থেকে Server Side অনুমোদন পাওয়ার জন্য CORS Middleware for Express */
+const cors = require("cors");
+app.use(cors());
+/** Client Side থেকে Data JSON stringify হয়ে আসার সময় JSON Parse করতে হয়  */
+app.use(express.json());
+
+// simpleDBUser
+// 6FLzph8cCiKYrtvw
+const uri =
+  "mongodb+srv://simpleDBUser:6FLzph8cCiKYrtvw@cluster0.mrg0zof.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+/* Express Route */
+app.get("/", (req, res) => {
+  res.send("Simple crud Server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Simple crud Server is running on port ${port}`);
+});
